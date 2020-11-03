784b01296936e51bb085e2af70fbd3758a0975c3
Alusbec <adrienlsbc@gmail.com>
2020-11-03 16:22:37 +0100

    correction

--- asset/JS/style.js
+++ asset/JS/style.js
@@ -1,9 +1,20 @@
-document.getElementById('clickbutton').onclick = showAlert;
-function showAlert(){
+document.getElementById('clickButton').onclick = showAlert; // appel de fonction au clic sur l'ID clickbutton
+function showAlert() { // prend les valeurs et calcul le resultat
+    var shoeSize = document.getElementById('shoeSize').value; // declare variable shoeSize
+    var birth = document.getElementById('birth').value; // declare variable birth
+    var result = ((shoeSize * 2 + 5) * 50 - birth + 1769); // declare variable calcul
 
-var pointure = document.getElementById('pointure').value;
-var naissance = document.getElementById('naissance').value;
-var result = ((pointure*2+5)*50-naissance+1769);
+    var result2 = shoeSize * 2; 
+        result2 = result2 + 5;
+        result2 = result2 * 50;
+        result2 = result2 - birth;
+        result2 = result2 + 1769;
 
-alert('Result :' +result);
+    var result3 = shoeSize * 2;
+        result3 += 5;
+        result3 *= 50;
+        result3 -= birth;
+        result3 += 1769;
+
+    alert('Result :' + result);
 }
